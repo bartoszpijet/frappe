0ad2e01889b30f0d6609e63116426601d0290b9f
hubtom <hubtom123@gmail.com>
2025-03-24 14:06:49 +0100

    outlook changes

--- frappe/integrations/doctype/outlook_calendar/outlook_calendar.py
+++ frappe/integrations/doctype/outlook_calendar/outlook_calendar.py
@@ -3,4 +3,22 @@ from frappe import _
 from frappe.model.document import Document
 
 class OutlookCalendar(Document):
-	pass
+	# begin: semi-auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		authorization_code: DF.Password | None
+		calendar_name: DF.Data
+		enable: DF.Check
+		outlook_calendar_id: DF.Data | None
+		next_sync_token: DF.Password | None
+		pull_from_outlook_calendar: DF.Check
+		sync_as_public: DF.Check
+		push_to_outlook_calendar: DF.Check
+		refresh_token: DF.Password | None
+		user: DF.Link
+	# end: semi-auto-generated types
